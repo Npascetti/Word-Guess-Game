402575643149a1b64894a0313aca4b744de18b11
Jesse Schimmel <jhowardschimmel@gmail.com>
2018-08-26 19:30:18 -0400

    Basic HTML and keyboard js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//Keyboard code goes here.
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+
+// Determines which key was pressed and stores it to a variable userPress.
+var userPress = event.key.toLowerCase();
+}
+
+//Write userPress to the console for testing.
+console.log(userPress);
+//Global variables go here.
+
+//Game object goes here.
+
+//Calls go here.
